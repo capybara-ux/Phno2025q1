82945ebac7d4bcbd4132b2f6e972eaeb1df37bb6
Имя пользователя < >
2025-09-25 18:34:29 +0300

    add : создал 1 функцию

---
+++ function.js
@@ -0,0 +1,4 @@
+const multiply = (x,y)=>{
+    return x*y
+}
+    console.log(multiply(5,6765))
---
+++ var.js
@@ -0,0 +1,5 @@
+const  firstnumber = 5
+const secondnumber = 3
+const result = firstnumber*secondnumber
+console.log(result)
+
